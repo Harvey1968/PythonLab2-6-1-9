a0b2249723680ad7597bb20f954a84a5089f4c08
Harvey1968 <108738675+Harvey1968@users.noreply.github.com>
2022-07-27 11:53:15 +0100

    Add files via upload

---
+++ PythonLab2-6-1-9/PythonLab2-6-1-9/PythonLab2_6_1_9.py
@@ -0,0 +1,23 @@
+#Oridginal
+# input a float value for variable a here
+# input a float value for variable b here
+
+# output the result of addition here
+# output the result of subtraction here
+# output the result of multiplication here
+# output the result of division here
+
+#print("\nThat's all, folks!")
+
+#Scenario
+a = float(input("value for variable a: "))
+b = float(input("value for variable b: "))
+print()
+
+print("result of addition:       ", a + b)
+print("result of subtraction:    ", a - b)
+print("result of multiplication: ", a * b)
+print("result of division:       ", a / b)
+
+print("\nThat's all, folks!\n")
+
